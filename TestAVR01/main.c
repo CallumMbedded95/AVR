7ae24cc624b768a5e4bb305965451b8c296fae46
callum <callum@localhost.localdomain>
2020-09-23 20:56:13 +1000

    Reconfigured Make for 1MHz, LED flicker using bit shifts for clarity

--- TestAVR01/main.c
+++ TestAVR01/main.c
@@ -8,29 +8,37 @@
 #include <avr/io.h>
 #include <util/delay.h>
  
+#define LED_DDR 	DDRB
+#define LED_PORT 	PORTB
+#define LED_PINS	PINB
+#define LED_01		3
+#define LED_02		4
  
 int main (void)
 {
   // set PB3 to be output
-	DDRB = 0b00001000;
+	//LED_DDR = 0b00011000; // Binary format
+	LED_DDR = 0x3f; // Hex format = 0b00111111
+	// LED_DDR = 0b00000000;
+	// LED_DDR = (1 < LED_01);
+	// LED_DDR |= (1 < LED_02);
   while (1) {
-    
-    // flash# 1:
-    // set PB3 high
-    PORTB = 0b00001000; 
-    _delay_ms(20);
-    // set PB3 low
-    PORTB = 0b00000000;
+	// set PB3 low
+    PORTB = (1 << LED_01); 
+    _delay_ms(150);
+
+    PORTB = 0x0;
     _delay_ms(20);
 
-    // flash# 2:
-    // set PB3 high
-    PORTB = 0b00001000; 
-    _delay_ms(200);
-    // set PB3 low
-    PORTB = 0b00000000;
-    _delay_ms(200);
+    PORTB = (1 << LED_02);  
+    _delay_ms(50);
+
+    PORTB |= (1 << LED_01);
+    _delay_ms(100);
+
+    PORTB = 0x00;
+    _delay_ms(1000);
   }
  
-  return 1;
+  return 0;
 }
